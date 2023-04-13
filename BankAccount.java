11b717b26d8de6205793dffddd2cad95c9e610d6
kishore-klassy <94288452+kishore-klassy@users.noreply.github.com>
2023-04-14 00:12:25 +0530

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -3,18 +3,19 @@ import java.lang.*;
 import java.io.*;
 class BankAccount{
 	Scanner sc = new Scanner(System.in);
-	private double account_number;
+	private long account_number;
 	private String name;
-	private double balance;
+	private long balance;
 	
-	BankAccount(double account_number,String name,double balance){
+	BankAccount(long account_number,String name,long balance){
 		this.account_number=account_number;
 		this.name = name;
 		this.balance=balance;
 	}
 	
 	public void chooseOption(){	
-		for(int i = 0 ;i<=5;i++){
+		int t=5 ;
+		do{
 		System.out.println("1.DEPOSIT");
 		System.out.println("2.WITHDRAW");
 		System.out.println("3.BALANCE");
@@ -23,7 +24,7 @@ class BankAccount{
 		
 		int option = sc.nextInt();
 		switch(option){
-			case 1 : this.deposite();
+			case 1 : this.deposit();
 					 break ;
 			case 2 : this.withdraw() ;
 					 break ;
@@ -31,21 +32,23 @@ class BankAccount{
 					 break ;
 			case 4 : return ;
 		}
+		t-- ;
 		}
+		while(t>=0) ;
 	}
-	public double deposite(){
+	public long deposit(){
 		
 		System.out.println("Enter amount to DEPOSIT : ");
-		double depositAmount = sc.nextInt();
+		long depositAmount = sc.nextInt();
 		balance = depositAmount + this.balance ;
 		return balance ;
 	}
 	
 	
-	public double withdraw(){
+	public long withdraw(){
 		
 		System.out.println("Enter amount to WITHDRAW: ");
-		double withdrawAmount = sc.nextInt();
+		long withdrawAmount = sc.nextInt();
 		balance = this.balance - withdrawAmount ;
 		return withdrawAmount ;
 		
